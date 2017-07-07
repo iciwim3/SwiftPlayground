5a9a3bfbe0fb4394fea7bfbb8ee7d8dfe7dde575
Bernard Huff <bernard.huff008@gmail.com>
2017-07-07 10:30:20 -0500

    Here is the first commit.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+
+//What up Bro, this is where we can practice what we learn.
+
+import UIKit
+
+var str = "Hello, playground"
+
+var name = "Bernard" 
