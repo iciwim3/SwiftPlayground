11fef431f524a47fc04ad39820631dc9d2f206b7
Bernard Huff <bernard.huff008@gmail.com>
2017-07-22 19:37:59 -0500

    Added some switches and options to see how it works.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,3 +13,27 @@ func staticShock() {
 }
 
 staticShock()
+
+let vegetable = "red pepper"
+
+switch vegetable {
+    
+    case "celery":
+    
+        print("Add some raisins and make ants on a log.")
+    
+    case "cucumber", "watercress":
+    
+        print("That would make a good tea sandwich.")
+    
+    default:
+    
+        print("Everything tastes good in soup.")
+    
+}
+
+
+
+
+
+
