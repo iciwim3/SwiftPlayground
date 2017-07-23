64fed3f27a09c5e89a2158abd6da77e8b9aed35d
Bernard Huff <bernard.huff008@gmail.com>
2017-07-22 19:54:08 -0500

    Check out the indents tho. Dope!!

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -32,8 +32,33 @@ switch vegetable {
     
 }
 
+// Here im playing with some different engineering concepts 
 
+let interestingNumbers = [
 
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Stupid": [1, 5, 3, 7, 10],
+    "Square": [8, 3, 6, 1, 20]
+
+]
+
+var largest = 0
+
+for (kind, numbers) in interestingNumbers {
+
+    for number in numbers {
+    
+        if number > largest {
+        
+            largest = number
+            
+        }
+        
+    }
+    
+}
+
+print(largest)
 
 
 
